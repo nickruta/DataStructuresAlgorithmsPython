494bacaf3f342e58e332626f725959270b56aeca
nickruta <nickruta@gmail.com>
2018-03-11 20:29:06 -0400

    code design updates made to be pep8 compliant

--- stack_paren_checker.py
+++ stack_paren_checker.py
@@ -1,18 +1,22 @@
+""" Using the PEP 8 Style Guide for Python Code:
+https://www.python.org/dev/peps/pep-0008/ and python linters """
+
 from stack import Stack
 
-def parChecker(symbolString):
-    """ This function, parChecker, assumes that a Stack class is available and 
-    returns a boolean result as to whether the string of parentheses is 
-    balanced. 
 
-    Note: the boolean variable balanced is initialized to True as there is no 
-    reason to assume otherwise at the start. If the current symbol is (, then it
-     is pushed on the stack (lines 9–10). Note also in line 15 that pop simply 
-     removes a symbol from the stack. The returned value is not used since we 
-     know it must be an opening symbol seen earlier. At the end (lines 19–22), 
-     as long as the expression is balanced and the stack has been completely 
-     cleaned off, the string represents a correctly balanced sequence of 
-     parentheses.
+def parChecker(symbolString):
+    """ This function, parChecker, assumes that a Stack class is available and
+    returns a boolean result as to whether the string of parentheses is
+    balanced.
+
+    Note: the boolean variable balanced is initialized to True as there is no
+    reason to assume otherwise at the start. If the current symbol is (, then
+    it is pushed on the stack (lines 9–10). Note also in line 15 that pop
+    simply removes a symbol from the stack. The returned value is not used
+    since we know it must be an opening symbol seen earlier. At the end
+    (lines 19–22), as long as the expression is balanced and the stack has
+    been completely cleaned off, the string represents a correctly balanced
+    sequence of parentheses.
 
     Args:
         symbolString (str) : The string of parenthesis to be parsed.
@@ -39,6 +43,7 @@ def parChecker(symbolString):
         return True
     else:
         return False
+
 
 print(parChecker('((()))'))
 print(parChecker('(()'))
